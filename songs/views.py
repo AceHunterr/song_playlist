7e219c76c3ad870e397a935557cfe3fd7bcc340b
MehulAgg26 <21BMA003@nith.ac.in>
2022-06-24 03:03:44 +0530

    Finished front page and all_songs page styling

--- songs/views.py
+++ songs/views.py
@@ -26,19 +26,165 @@ all_categories = [
   }
 ]
 
+songs_list =[
+  {
+    "slug" : None,
+    "image": "song_img1.jpg",
+    "name": "Song Title",
+    "artist": "Artist 1",
+    "genre": "Genre 1",
+    "language" : "English"
+  },
+
+    {
+    "slug" : None,
+    "image": "song_img2.jpg",
+    "name": "Song Title",
+    "artist": "Artist 2",
+    "genre": "Genre 2",
+    "language" : "English"
+  },
+    {
+    "slug" : None,
+    "image": "song_img3.jpg",
+    "name": "Song Title",
+    "artist": "Artist 1",
+    "genre": "Genre 3",
+    "language" : "Hindi"
+  },
+    {
+    "slug" : None,
+    "image": "artist.jpg",
+    "name": "Song Title",
+    "artist": "Artist 3",
+    "genre": "Genre 2",
+    "language" : "Spanish"
+  },
+    {
+    "slug" : None,
+    "image": "language.jpg",
+    "name": "Song Title",
+    "artist": "Artist 2",
+    "genre": "Genre 4",
+    "language" : "Japanese"
+  },
+    {
+    "slug" : None,
+    "image": "song_img3.jpg",
+    "name": "Song Title",
+    "artist": "Artist 1",
+    "genre": "Genre 1",
+    "language" : "Hindi"
+  },
+    {
+    "slug" : None,
+    "image": "artist.jpg",
+    "name": "Song Title",
+    "artist": "Artist 4",
+    "genre": "Genre 2",
+    "language" : "English"
+  },
+    {
+    "slug" : None,
+    "image": "artist1.webp",
+    "name": "Song Title",
+    "artist": "Artist 3",
+    "genre": "Genre 2",
+    "language" : "Hindi"
+  },
+    {
+    "slug" : None,
+    "image": "language1.png",
+    "name": "Song Title",
+    "artist": "Artist 1",
+    "genre": "Genre 1",
+    "language" : "English"
+  }
+]
+
+carousel_list = [
+  {
+    "id" : "carousel-1",
+  },
+  {
+    "id" : "carousel-2",
+  },
+  {
+    "id" : "carousel-3",
+  }
+]
+
+
+
+artists = []
+
+
+
+def sorting_songs(category,songs_list):
+  """Function to sort and make list of songs category wise"""
+  cat_items = []
+  for song in songs_list:
+    for key,value in song.items():
+      if key==category:
+        if value not in cat_items:
+          cat_items.append(value)  
+
+  category_sorting_list = []
+  for cat_item in cat_items:
+    single_cat_songs = []
+    for song in songs_list:
+      for key,value in song.items():  
+        if key==category and value == cat_item:
+          single_cat_songs.append(song)
+    category_sorting_list.append({"key":cat_item,"value":single_cat_songs})  
+
+  return category_sorting_list      
+
+
+songs_list_by_language = sorting_songs("language",songs_list)
+songs_list_by_genre = sorting_songs("genre",songs_list)
+songs_list_by_artist = sorting_songs("artist",songs_list)
+
+
+
+
+# for song in songs_list:
+#   for key,value in song.items():
+#     if key=="artist":
+#       if value not in artists:
+#         artists.append(value)
+
+# for artist in artists:
+#   single_artist_songs = []
+#   for song in songs_list:
+#     for key,value in song.items():  
+#       if key=="artist" and value == artist:
+#         single_artist_songs.append(song)
+#   songs_list_by_artist.append({artist:single_artist_songs})
+
+
 def index(request):
   return render(request , "songs/index.html",{
     "all_categories" : all_categories
   })
 
 def songs_by_language(request):
-  return render(request, "songs/songs_by_language.html")
+  return render(request, "songs/songs_by_language.html",{
+  "sorted_list":songs_list_by_language}
+  )
 
 def songs_by_genre(request):
-  return render(request, "songs/songs_by_genre.html")
+  return render(request, "songs/songs_by_genre.html",{
+    "sorted_list":songs_list_by_genre
+  })
 
 def songs_by_artist(request):
-  return render(request, "songs/songs_by_artist.html")
+  return render(request, "songs/songs_by_artist.html",{
+  "sorted_list" : songs_list_by_artist}
+  )
 
 def all_songs(request):
-  return render(request, "songs/all_songs.html")
+  return render(request, "songs/all_songs.html",{
+    "songs_list" : songs_list,
+    "carousel_list" : carousel_list
+  })
