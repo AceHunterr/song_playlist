1ed602b40b398de3b1f9a44b258a53cbfd1c0d58
MehulAgg26 <21BMA003@nith.ac.in>
2022-06-26 23:05:07 +0530

    Added Models and superuser

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Song
 
+class SongAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("name",)}
+  # list_filter = ("artist")
+admin.site.register(Song,SongAdmin)
 # Register your models here.
--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.utils.text import slugify
 
 # Create your models here.
+
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z -]*$', 'Only alphanumeric characters are allowed.')
+
+
+class Song(models.Model):
+  name = models.CharField(max_length=30,validators=[alphanumeric])
+  slug=models.SlugField(default="",blank=True,null=False,db_index=True)
+  artist = models.CharField(max_length=30,validators=[alphanumeric])
+  genre = models.CharField(max_length=30,validators=[alphanumeric])
+  language = models.CharField(max_length=30,validators=[alphanumeric])
+  image = models.ImageField(null=True,blank=True)
+  
+  def save(self, *args, **kwargs):
+    self.slug = slugify(self.name)
+    super().save(*args,**kwargs)
+
+  def __str__(self):
+    return f"{self.name} by {self.artist}"
